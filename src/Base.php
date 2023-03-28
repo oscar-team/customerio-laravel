648206780566645ad2477b5cadd1506dde1dabad
Babar Afzaal <57573892+babarmalik6444@users.noreply.github.com>
2023-03-28 10:11:56 +0200

    Updated class according to PSR 12

--- src/Base.php
+++ src/Base.php
@@ -3,10 +3,24 @@ namespace Oscar\CustomerioLaravel;
 
 use Customerio\Client;
 
+/**
+ * This is the Base class for CustomerIoLaravel package.
+ * It initializes a client object for Customer.io API calls using the provided credentials.
+ */
+
 class Base {
 
+    /**
+     * The client object used for making API calls.
+     *
+     * @var Client
+     */
     protected $client;
 
+    /**
+     * Create a new instance of Base class and initialize the client object.
+     */
+    
     public function __construct() {
         $this->client = new Client(config('customerio.api_key'), config('customerio.site_id'));
         $this->client->setAppAPIKey(config('customerio.app_api_key'));
--- src/CustomerIo.php
+++ src/CustomerIo.php
@@ -3,133 +3,89 @@ namespace Oscar\CustomerioLaravel;
 use Oscar\CustomerioLaravel\Base;
 
 
-class CustomerIo extends Base {
-
-    public function __construct() {
+/**
+ * The CustomerIo class is a wrapper for the Customer.io API that provides a set of
+ * methods for managing customers and events.
+ */
+class CustomerIo extends Base
+{
+    /**
+     * Create a new instance of the CustomerIo class.
+     */
+    public function __construct()
+    {
         parent::__construct();
     }
 
-    public function searchCustomerByEmail(string $email) {
-            $response = $this->client->customers->get(
-                [
-                    'email' => $email,
-                ]
-            );
+    /**
+     * Search for a customer by email.
+     *
+     * @param string $email The email address to search for.
+     * @return bool True if the customer was found, false otherwise.
+     */
+    public function searchCustomerByEmail(string $email): bool
+    {
+        $response = $this->client->customers->get([
+            'email' => $email,
+        ]);
+
         return $response->results ? true : false;
     }
 
-
-    public function addCustomer(array $customer) {
-        $response = $this->client->customers->add(
-            [
-                'id' => $customer['id'],
-                'email' => $customer['email'],
-                'first_name' => $customer['first_name'],
-                'last_name' => $customer['last_name'],
-                'business_name' => $customer['business_name'],
-                'phone' => $customer['phone'],
-                'birthday' => $customer['birthday'],
-                'accepts_marketing' => $customer['accepts_marketing'],
-                'market' => $customer['market'],
-                'preferred_language' => $customer['preferred_language'],
-                'total_spend' => $customer['total_spend'],
-                'number_of_bookings' => $customer['number_of_bookings'],
-            ]
-        );
+    /**
+     * Add a new customer.
+     *
+     * @param array $customer An array of customer data.
+     * @return mixed The response from the API.
+     */
+    public function addCustomer(array $customer)
+    {
+        $response = $this->client->customers->add($customer);
 
         return $response;
     }
 
-    public function updateCustomer(array $customer) {
-        $response = $this->client->customers->update(
-            [
-                'id' => $customer['id'],
-                'email' => $customer['email'],
-                'first_name' => $customer['first_name'],
-                'last_name' => $customer['last_name'],
-                'business_name' => $customer['business_name'],
-                'phone' => $customer['phone'],
-                'birthday' => $customer['birthday'],
-                'accepts_marketing' => $customer['accepts_marketing'],
-                'market' => $customer['market'],
-                'preferred_language' => $customer['preferred_language'],
-                'total_spend' => $customer['total_spend'],
-                'number_of_bookings' => $customer['number_of_bookings'],
-            ]
-        );
+    /**
+     * Update an existing customer.
+     *
+     * @param array $customer An array of customer data.
+     * @return mixed The response from the API.
+     */
+    public function updateCustomer(array $customer)
+    {
+        $response = $this->client->customers->update($customer);
 
         return $response;
     }
 
-    public function deleteCustomer(int $id) {
-        $response = $this->client->customers->delete(
-            [
-                'id' => $id
-            ]
-        );
+    /**
+     * Delete a customer.
+     *
+     * @param int $id The ID of the customer to delete.
+     * @return mixed The response from the API.
+     */
+    public function deleteCustomer(int $id)
+    {
+        $response = $this->client->customers->delete([
+            'id' => $id,
+        ]);
 
         return $response;
     }
 
-    public function bookingCreatedEvent(array $booking) {
-        $response = $this->client->customers->event(
-            [
-                'id' => $booking['customer_id'],
-                'name' => 'Booking Created',
-                'data' => [
-                    'email' => $booking['email'],
-                    'booking_id' => $booking['booking_id'],
-                    'booking_created' => $booking['created_at'],
-                    'booking_start_date_time' => $booking['pickup_at'],
-                    'booking_return_date_time' => $booking['delivery_at'],
-                    'booking_duration' => $booking['booking_duration'],
-                    'booking_source' => $booking['booking_source'],
-                    'department_id' => $booking['department_id'],
-                    'department_name' => $booking['department_name'],
-                    'car_id' => $booking['car_id'],
-                    'car_type' => $booking['car_type'],
-                    'market' => $booking['market'],
-                    'preferred_language' => $booking['preferred_language'],
-                    'booking_total_amount' => $booking['booking_total_amount'],
-                    'total_spend' => $booking['total_spend'],
-                    'number_of_bookings' => $booking['number_of_bookings'],
-                ]
-            ]
-        );
-        return $response;
-    }
+    /**
+     * Create a new event.
+     *
+     * @param array $data An array of event data.
+     * @return mixed The response from the API.
+     */
+    public function createEvent(array $data)
+    {
+        $response = $this->client->customers->event($data);
 
-    public function bookingCanceledOrDeletedEvent(array $booking) {
-        $response = $this->client->customers->event(
-            [
-                'id' => $booking['customer_id'],
-                'name' => 'Booking '. ($booking['is_cancelled'] ? 'Cacelled' : 'Deleted') ,
-                'data' => [
-                    'email' => $booking['email'],
-                    'booking_id' => $booking['booking_id'],
-                    'booking_canceled' => $booking['canceled_at'],
-                ]
-            ]
-        );
-        return $response;
-    }
-
-    public function bookingCompleted(array $booking) {
-        $response = $this->client->customers->event(
-            [
-                'id' => $booking['customer_id'],
-                'name' => 'Booking Completed',
-                'data' => [
-                    'email' => $booking['email'],
-                    'booking_id' => $booking['booking_id'],
-                    'booking_completed' => $booking['completed_at'],
-                ]
-            ]
-        );
         return $response;
     }
 }
-
 
 
 ?>
